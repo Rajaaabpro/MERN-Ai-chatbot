2575edfa6f9d52a4288e6d1517eaba6fb5091c30
Rajaaabpro <rajaab.pro@gmail.com>
2024-05-20 10:17:48 +0500

    created CustomizedInput

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,55 +1,57 @@
 import React from "react";
-import { AppBar } from "@mui/material";
-import Toolbar from "@mui/material/Toolbar";
+import { AppBar, Box, Toolbar } from "@mui/material";
 import Logo from "./shared/Logo";
 import NavigationLink from "./shared/NavigationLink";
 import { useAuth } from "../context/AuthContext";
 
-const Header = () => {
+function Header() {
   const auth = useAuth();
+
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
-      <Toolbar sx={{ display: "flex" }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         <Logo />
         <div>
-          {auth?.isLoggedIn ? (
-            <>
-              <NavigationLink
-                bg="#00fffc"
-                to="/Chat"
-                text="Go To Chat"
-                textcolor="black"
-              />
-              <NavigationLink
-                bg="#51538f"
-                textcolor="white"
-                to="/"
-                text="logout"
-                onclick={auth.logout}
-              />
-            </>
-          ) : (
-            <>
-              <NavigationLink
-                bg="#00fffc"
-                to="/login"
-                text="Login"
-                textcolor="black"
-              />
-              <NavigationLink
-                bg="#51538f"
-                textcolor="white"
-                to="/signup"
-                text="Signup"
-              />
-            </>
-          )}
+          <Box sx={{ display: "flex", gap: 2 }}>
+            {auth?.isLoggedIn ? (
+              <>
+                <NavigationLink
+                  bg="#00fffc"
+                  to="/chat"
+                  text="Go To Chat"
+                  textcolor="black"
+                />
+                <NavigationLink
+                  bg="#51538f"
+                  textcolor="white"
+                  to="/"
+                  text="Logout"
+                  onclick={auth.logout}
+                />
+              </>
+            ) : (
+              <>
+                <NavigationLink
+                  bg="#00fffc"
+                  to="/login"
+                  text="Login"
+                  textcolor="black"
+                />
+                <NavigationLink
+                  bg="#51538f"
+                  textcolor="white"
+                  to="/signup"
+                  text="Signup"
+                />
+              </>
+            )}
+          </Box>
         </div>
       </Toolbar>
     </AppBar>
   );
-};
+}
 
 export default Header;
