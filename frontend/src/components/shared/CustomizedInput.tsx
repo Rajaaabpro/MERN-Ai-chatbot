216e5e1f6d13aee11800fd07575cad3d106adbd1
Rajaaabpro <rajaab.pro@gmail.com>
2024-05-20 10:18:18 +0500

    r

--- frontend/src/components/shared/CustomizedInput.tsx
+++ frontend/src/components/shared/CustomizedInput.tsx
@@ -1,12 +1,13 @@
 import Recat from "react";
+import { TextField } from "@mui/material";
 type Props = {
     name: string;
     type: string;
     label: string;
 };
 
-const CustomizedInput = () => {
-    return <div>CustomizedInput</div>;
+const CustomizedInput = (props: Props) => {
+    return <TextField name = {props.name} label={props.label} type={props.type}/>;
 };
 
 export default CustomizedInput;
