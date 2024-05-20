5747c300f1240e20a2b0a1d04eb47f3f4247c163
Rajaaabpro <rajaab.pro@gmail.com>
2024-05-20 10:08:27 +0500

    created new file

---
+++ frontend/src/components/shared/CustomizedInput.tsx
@@ -0,0 +1,12 @@
+import Recat from "react";
+type Props = {
+    name: string;
+    type: string;
+    label: string;
+};
+
+const CustomizedInput = () => {
+    return <div>CustomizedInput</div>;
+};
+
+export default CustomizedInput;
