afe0ee480938e7cd584c94d917aabadeaa1ad63c
Rajaaabpro <rajaab.pro@gmail.com>
2024-05-22 05:12:49 +0500

    created all errors

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -1,4 +1,7 @@
+import { rejects } from "assert";
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { resolve } from "path";
 
 export const createToken = (id: String, email: string, expiresIn: string) => {
   const payload = { id, email };
@@ -6,4 +9,28 @@ export const createToken = (id: String, email: string, expiresIn: string) => {
     expiresIn,
   });
   return token;
+};
+
+export const verifyToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies["${COOKIE_NAME}"];
+  if (!token || token.trim() === "") {
+    return res.status(401).json({ message: "Token Not Received"});
+  }
+  return new Promise<void>((resolve, reject) => {
+    return jwt.verify(token, process.env.Jwt_SECRET,(err, success) => {
+      if (err) {
+        reject( err.message);
+        return res.status(401).json({Message:"Token Expired "});
+      } else {
+        console.log("Token Verification  Successful");
+        resolve();
+        res.locals.jwtData = success;
+        return next ();
+      }
+    });
+  });
 };
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { AppBar, Box, Toolbar } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
 import Logo from "./shared/Logo";
-import NavigationLink from "./shared/NavigationLink";
 import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
-function Header() {
+const Header = () => {
   const auth = useAuth();
 
   return (
@@ -13,45 +14,42 @@ function Header() {
     >
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         <Logo />
-        <div>
-          <Box sx={{ display: "flex", gap: 2 }}>
-            {auth?.isLoggedIn ? (
-              <>
-                <NavigationLink
-                  bg="#00fffc"
-                  to="/chat"
-                  text="Go To Chat"
-                  textcolor="black"
-                />
-                <NavigationLink
-                  bg="#51538f"
-                  textcolor="white"
-                  to="/"
-                  text="Logout"
-                  onclick={auth.logout}
-                />
-              </>
-            ) : (
-              <>
-                <NavigationLink
-                  bg="#00fffc"
-                  to="/login"
-                  text="Login"
-                  textcolor="black"
-                />
-                <NavigationLink
-                  bg="#51538f"
-                  textcolor="white"
-                  to="/signup"
-                  text="Signup"
-                />
-              </>
-            )}
-          </Box>
-        </div>
+        <Box sx={{ display: "flex", gap: 2 }}>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/chat"
+                text="Go To Chat"
+                textcolor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textcolor="white"
+                to="/"
+                text="Logout"
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/login"
+                text="Login"
+                textcolor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textcolor="white"
+                to="/signup"
+                text="Signup"
+              />
+            </>
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   );
-}
+};
 
 export default Header;
