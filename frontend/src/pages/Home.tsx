39fabe460227609cf55e080e3292449f9572647d
Rajaaabpro <rajaab.pro@gmail.com>
2024-05-26 13:16:29 +0500

    Created Animation in home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { Box } from "@mui/material";
+import TypingAnim from "../components/typer/TypingAnim";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box width={"100%"} height={"100%"}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          mx: "auto",
+        }}
+      >
+        <Box>
+          <TypingAnim />
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: { md: "row", xs: "column" },
+            gap: 5,
+            my: 10,
+          }}
+        >
+          <img
+            src="robot.png"
+            alt="robot"
+            style={{ width: "200px", margin: "auto" }}
+          />
+          <img
+          className="image-inverted"
+            src="openai.png"
+            alt="openai"
+            style={{ width: "200px", margin: "auto" }}
+            />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Home;
